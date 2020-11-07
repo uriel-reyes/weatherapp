810e147fbeda6e53f48785dd10c36b65c0bb79d9
Uriel Reyes <uriel.reyes@uri.local>
2020-11-06 21:20:51 -0600

    pulling weather data

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //used to import request, also need npm install request
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=7e47d511e8e8942951b4348b80eed067&query=Austin';
+const url = 'http://api.weatherstack.com/forecast?access_key=7e47d511e8e8942951b4348b80eed067&query=Austin&units=f';
 
-//boilerplate for a simple request, body is what displays the result
-request(url, (error, response) =>{
-//we create a const to store the response, and also parse it (the data we need lives inside the body property)    
-    const data = JSON.parse(response.body);
-    console.log(data.current);
-})
+//boilerplate for a simple request, json:true will parse the object into JSON
+request({url:url, json:true}, (error, response) =>{
+//we can now call response.body and a property of the object
+console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out and it feels like ' + response.body.current.feelslike + ' degrees.');
+
+});
+
+
